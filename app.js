8eb43c75ea0b62a5ab87dd7689f5af489d173be4
shariful islam <sharifulb07@gmail.com>
2023-09-18 16:19:51 +0600

    nodejs module system

---
+++ app.js
@@ -0,0 +1,4 @@
+const tutorial=require('./tutorial');
+console.log(tutorial.PI);
+console.log(tutorial.sum(10,20));
+console.log(new tutorial.SomeMathObject());
---
+++ tutorial.js
@@ -0,0 +1,8 @@
+const sum=(num1,num2)=>num1+num2;
+const PI=3.24;
+class SomeMathObject{
+    constructor(){
+        console.log('object created here ')
+    }
+}
+module.exports={sum:sum, PI:PI, SomeMathObject:SomeMathObject};
