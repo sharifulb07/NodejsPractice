fb40f10f673691cdc5de08130586c9275041ee25
shariful islam <sharifulb07@gmail.com>
2023-09-18 16:39:13 +0600

    Event and Eventemitter

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-const tutorial=require('./tutorial');
-console.log(tutorial.PI);
-console.log(tutorial.sum(10,20));
-console.log(new tutorial.SomeMathObject());
+const EventEmitter=require('events');
+const eventEmitter=new EventEmitter();
+
+eventEmitter.on('tutorial',()=>{
+    console.log('tutorial event has occurred');
+})
+
+eventEmitter.emit('tutorial',1,2);
+
+
+class Person extends EventEmitter{
+    constructor(name){
+        super();
+        this._name=name;
+    }
+
+    get name(){
+        return this._name;
+    }
+}
+
+let Sharif=new Person("Shariful Islam");
+let Adiayt=new Person("Hi Adiyat");
+
+Adiayt.on('name',()=>{
+    console.log('My name is '+Adiayt.name);
+});
+Sharif.on('name',()=>{
+    console.log('My name is '+Sharif.name);
+});
+
+Sharif.emit('name');
+Adiayt.emit('name');
