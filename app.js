0669e920655d93511215cce711eb1f693578a6a9
shariful islam <sharifulb07@gmail.com>
2023-09-18 21:30:33 +0600

    Create question and answer with readline package

--- app.js
+++ app.js
@@ -1,33 +1,71 @@
-const EventEmitter=require('events');
-const eventEmitter=new EventEmitter();
+// two modular way 
+const readline=require('readline');
+const rl=readline.createInterface({input:process.stdin, output:process.stdout});
 
-eventEmitter.on('tutorial',()=>{
-    console.log('tutorial event has occurred');
-})
-
-eventEmitter.emit('tutorial',1,2);
-
-
-class Person extends EventEmitter{
-    constructor(name){
-        super();
-        this._name=name;
-    }
-
-    get name(){
-        return this._name;
-    }
+const generateRan=()=>{
+    return Math.floor((Math.random()*10)+1);
+    
 }
 
-let Sharif=new Person("Shariful Islam");
-let Adiayt=new Person("Hi Adiyat");
+const ansQuestion=()=>{
+    let num1=generateRan();
+    let num2=generateRan();
+    let answer=num1+num2;
 
-Adiayt.on('name',()=>{
-    console.log('My name is '+Adiayt.name);
-});
-Sharif.on('name',()=>{
-    console.log('My name is '+Sharif.name);
-});
+    rl.question(`What is ${num1}+${num2} \n`, (userInput)=>{
+        if(userInput.trim()==answer){
+            rl.close();
 
-Sharif.emit('name');
-Adiayt.emit('name');
+        }else{
+            rl.setPrompt(`Incorrect answer try again `);
+            rl.prompt();
+            ansQuestion();
+        }
+
+    })
+}
+
+ansQuestion();
+
+rl.on('close',()=>{
+    console.log('Correct. Thanks you for your response');
+
+})
+
+
+
+
+// one declarative way 
+// const { userInfo } = require('os');
+// const readline=require('readline');
+// const rl=readline.createInterface({input:process.stdin, 
+// output:process.stdout
+// });
+
+// let num1=Math.floor((Math.random()*10)+1);
+// let num2=Math.floor((Math.random()*10)+1);
+
+// let answer=num1+num2;
+
+// rl.question(`What is ${num1}+${num2} \n`,(userInput)=>{
+//     if(userInput.trim()==answer){
+//         rl.close();
+//     }else{
+//         rl.setPrompt('InValid answer try again \n');
+//         rl.prompt();
+//         rl.on('line',(userInput)=>{
+//             if(userInput.trim()==answer){
+//                 rl.close();
+//             }else{
+//                 rl.setPrompt(`Your answer is ${userInput}: incorrect \n`);
+//                 rl.prompt();
+//             }
+//         })
+//     }
+// });
+
+
+// rl.on('close',(userInput)=>{
+//     console.log('Correct answer');
+// })
+
