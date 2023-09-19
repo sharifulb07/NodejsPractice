f3973dc71aaadc861d6d3f07c7104736c35929e9
shariful islam <sharifulb07@gmail.com>
2023-09-19 19:20:57 +0600

    file readStream is created for test purpose

--- app.js
+++ app.js
@@ -1,59 +1,10 @@
+
+// readable and writable stream here 
+
+
 const fs=require('fs');
+const readStream=fs.createReadStream('./task.txt','utf-8');
 
-fs.readdir('./tutorial',(err,files)=>{
-    if(err){
-        console.log(err);
-    }else{
-        for(let file of files){
-            fs.unlink('./tutorial/'+file,(err)=>{
-                if(err){
-                    console.log(err);
-                }else{
-                    console.log('File is deleted successfully here ');
-                }
-            })
-        }
-    }
+readStream.on('data', (chunk)=>{
+    console.log(chunk);
 })
-
-
-
-
-
-
-
-
-
-
-
-
-// fs.unlink('./tutorial/exam.txt', (err)=>{
-//     if(err){
-//         console.log(err);
-//     }else{
-//         fs.mkdir('tutorial', (err)=>{
-//             if(err){
-//                 console.log(err);
-//             }else{
-             
-//                 console.log('file is created successfully! ');
-//             }
-//         })
-//     }
-// })
-
-
-
-// fs.mkdir('tutorial', (err)=>{
-//     if(err){
-//         console.log(err);
-//     }else{
-//         fs.writeFile('./tutorial/exam.txt', 'this is only for exam and practice', (err)=>{
-//             if(err){
-//                 console.log(err);
-//             }else{
-//                 console.log('file is created successfully now ');
-//             }
-//         })
-//     }
-// })
